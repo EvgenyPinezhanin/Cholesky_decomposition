08148bd17253b5ce506b4603de181774afe44067
EvgenyPinezhanin <pinezhanin@yandex.ru>
2024-05-05 00:10:46 +0300

    skeleton commit

---
+++ src/cholesky_decomposition.cpp
@@ -0,0 +1,5 @@
+#include <cholesky_decomposition.h>
+
+void Cholesky_Decomposition(double * A, double * L, int n) {
+
+}
---
+++ test/large_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <cholesky_decomposition.h>
+
+int main() {
+    std::cout << "Hello World!" << "\n";
+    return 0;
+}
